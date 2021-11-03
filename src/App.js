33eaf5bf95d69b94b8c6e56ca15dddea9f4c9671
yeonlisa <tkzw4869@gmail.com>
2021-11-03 11:24:56 +0900

    버튼에 기능개발을 해보자 & 리액트 state변경하는 법

--- src/App.js
+++ src/App.js
@@ -4,9 +4,8 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-
-  let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집']);
-
+  let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '리액트 독학']);
+  let [따봉, 따봉변경] = useState(0);
   let posts = '강남 고기 맛집';
 
   return (
@@ -15,8 +14,18 @@ function App() {
         <div>개발 Blog</div>
       </div>
       <div className="list">
-        <h3>{ 글제목[0] }</h3>
+        <h3>{ 글제목[0] } <span onClick={() => { 따봉변경(따봉+1) }}>👍</span> {따봉}</h3>
         <p>2월 17일 발행</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h3>{ 글제목[1] }</h3>
+        <p>2월 18일 발행</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h3>{ 글제목[2] }</h3>
+        <p>2월 19일 발행</p>
         <hr/>
       </div>
     </div>
