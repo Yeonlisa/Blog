8178fbdad2192c1bd347d730a28be92c92aafb02
yeonlisa <tkzw4869@gmail.com>
2021-11-03 11:26:48 +0900

    버튼에 기능개발을 해보자 & 리액트 state변경하는 법

--- src/App.js
+++ src/App.js
@@ -19,12 +19,12 @@ function App() {
         <hr/>
       </div>
       <div className="list">
-        <h3>{ 글제목[1] }</h3>
+        <h3>{ 글제목[1] } <span onClick={() => { 따봉변경(따봉+1) }}>👍</span> {따봉}</h3>
         <p>2월 18일 발행</p>
         <hr/>
       </div>
       <div className="list">
-        <h3>{ 글제목[2] }</h3>
+        <h3>{ 글제목[2] } <span onClick={() => { 따봉변경(따봉+1) }}>👍</span> {따봉}</h3>
         <p>2월 19일 발행</p>
         <hr/>
       </div>
