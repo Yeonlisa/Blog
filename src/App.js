794309aa442293d0f8a25c3bc16236df29b99f5e
yeonlisa <tkzw4869@gmail.com>
2021-12-04 18:17:27 +0900

    클릭하면 동작하는 UI (모달창) 만드는 법

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 function App() {
   let [글제목, 글제목변경] = useState(['남자코트 추천', '강남 우동맛집', '리액트 독학']);
   let [따봉, 따봉변경] = useState(0);
+  let [modal, modal변경] = useState(false);
   let posts = '강남 고기 맛집';
 
   function 제목바꾸기() {
@@ -19,7 +20,6 @@ function App() {
       <div className="black-nav">
         <div>개발 Blog</div>
       </div>
-      <button onClick={제목바꾸기}>버튼</button>
       <div className="list">
         <h3>{ 글제목[0] } <span onClick={() => { 따봉변경(따봉+1) }}>👍</span> {따봉}</h3>
         <p>2월 17일 발행</p>
@@ -31,11 +31,16 @@ function App() {
         <hr/>
       </div>
       <div className="list">
-        <h3>{ 글제목[2] }</h3>
+        <h3 onClick={() => { modal변경(true) }}>{ 글제목[2] }</h3>
         <p>2월 19일 발행</p>
         <hr/>
       </div>
-      <Modal />
+      <button onClick={() => { modal변경(!modal) }}>버튼</button>
+      {
+        modal === true 
+        ? <Modal /> 
+        : null
+      }
     </div>
   );
 }
