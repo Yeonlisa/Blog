31417caa04ef0a8a79fd09028a035308b762a70d
yeonlisa <tkzw4869@gmail.com>
2021-12-06 15:07:55 +0900

    map : 많은 div들을 반복문으로 줄이고 싶은 충동이 들 때

--- src/App.js
+++ src/App.js
@@ -7,35 +7,33 @@ function App() {
   let [글제목, 글제목변경] = useState(['남자코트 추천', '강남 우동맛집', '리액트 독학']);
   let [따봉, 따봉변경] = useState(0);
   let [modal, modal변경] = useState(false);
+  
   let posts = '강남 고기 맛집';
 
-  function 제목바꾸기() {
-    let newArray = [...글제목];
-    newArray[0] = '여자코트 추천';
-    글제목변경(newArray);
-  }
+  // function 제목바꾸기() {
+  //   let newArray = [...글제목];
+  //   newArray[0] = '여자코트 추천';
+  //   글제목변경(newArray);
+  // }
 
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 Blog</div>
       </div>
-      <div className="list">
-        <h3>{ 글제목[0] } <span onClick={() => { 따봉변경(따봉+1) }}>👍</span> {따봉}</h3>
-        <p>2월 17일 발행</p>
-        <hr/>
-      </div>
-      <div className="list">
-        <h3>{ 글제목[1] }</h3>
-        <p>2월 18일 발행</p>
-        <hr/>
-      </div>
-      <div className="list">
-        <h3 onClick={() => { modal변경(true) }}>{ 글제목[2] }</h3>
-        <p>2월 19일 발행</p>
-        <hr/>
-      </div>
-      <button onClick={() => { modal변경(!modal) }}>버튼</button>
+
+      {
+        글제목.map((글) => {
+          return (
+            <div className="list">
+              <h3>{ 글 } <span onClick={() => { 따봉변경(따봉+1) }}>👍</span> {따봉}</h3>
+              <p>2월 17일 발행</p>
+              <hr/>
+            </div>
+          )
+        })
+      }
+
       {
         modal === true 
         ? <Modal /> 
