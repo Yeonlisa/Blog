806d5cf2a507dc5a03a5bf8c56cb0eb6bb9f792e
yeonlisa <tkzw4869@gmail.com>
2021-12-16 16:41:12 +0900

    props : 자식이 부모의 state를 가져다쓰고 싶을 땐 말하고 쓰셔야합니다

--- src/App.js
+++ src/App.js
@@ -7,14 +7,6 @@ function App() {
   let [글제목, 글제목변경] = useState(['남자코트 추천', '강남 우동맛집', '리액트 독학']);
   let [따봉, 따봉변경] = useState(0);
   let [modal, modal변경] = useState(false);
-  
-  let posts = '강남 고기 맛집';
-
-  // function 제목바꾸기() {
-  //   let newArray = [...글제목];
-  //   newArray[0] = '여자코트 추천';
-  //   글제목변경(newArray);
-  // }
 
   return (
     <div className="App">
@@ -27,26 +19,28 @@ function App() {
           return (
             <div className="list">
               <h3>{ 글 } <span onClick={() => { 따봉변경(따봉+1) }}>👍</span> {따봉}</h3>
-              <p>2월 17일 발행</p>
+              <p>2월 18일 발행</p>
               <hr/>
             </div>
           )
         })
       }
 
+      <button onClick={ () =>{modal변경(!modal) }}>열고닫기</button>
+
       {
         modal === true 
-        ? <Modal /> 
+        ? <Modal 글제목={글제목}></Modal>
         : null
       }
     </div>
   );
 }
 
-function Modal() {
+function Modal(props) {
   return (
     <div className="modal">
-      <h2>제목</h2>
+      <h2>{ props.글제목[1] }</h2>
       <p>날짜</p>
       <p>상세내용</p>
     </div>
